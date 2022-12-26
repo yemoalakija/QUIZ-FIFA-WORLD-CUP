7d466ffbe170b9413e44cdb0f3e2965c1acf96f6
yemoalakija <adeyemo.alakija@gmail.com>
2022-12-26 23:15:10 +0000

    adds let and function elements to quiz.js

--- quiz.js
+++ quiz.js
@@ -65,3 +65,55 @@ const questions = [
       options: ["Morocco", "Qatar", "Senegal"],
     },
   ];
+
+  let question_count = 0;
+let points = 0;
+
+window.onload = function () {
+  show(question_count);
+};
+
+function show(count) {
+  let question = document.getElementById("questions");
+  let [first, second, third] = questions[count].options;
+
+  question.innerHTML = `<h2>Q${count + 1}. ${questions[count].question}</h2>
+    <ul class="option_group">
+    <li class="option">${first}</li>
+    <li class="option">${second}</li>
+    <li class="option">${third}</li>
+    </ul>`;
+  toggleActive();
+}
+
+function toggleActive() {
+  let option = document.querySelectorAll("li.option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].onclick = function () {
+      for (let i = 0; i < option.length; i++) {
+        if (option[i].classList.contains("active")) {
+          option[i].classList.remove("active");
+        }
+      }
+      option[i].classList.add("active");
+    };
+  }
+}
+
+function next() {
+  if (question_count == questions.length - 1) {
+    location.href = "result.html";
+  }
+  console.log(question_count);
+
+  let user_answer = document.querySelector("li.option.active").innerHTML;
+
+  if (user_answer == questions[question_count].answer) {
+    points += 1;
+    sessionStorage.setItem("points", points);
+  }
+  console.log(points);
+
+  question_count++;
+  show(question_count);
+}

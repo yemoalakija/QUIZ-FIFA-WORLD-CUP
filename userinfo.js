1f3917c168e62ab8aff4feb0e48aaf14b785dfd1
yemoalakija <adeyemo.alakija@gmail.com>
2023-02-25 11:07:57 +0000

    adds event listener and document query selector to jscode

--- userinfo.js
+++ userinfo.js
@@ -1,8 +1,14 @@
 // Player details
+// This ensures that the JavaScript code is executed after the HTML document has finished
+// loading, so the elements with the "name" and "points" class names are available in the DOM.
 
-let user_name = sessionStorage.getItem("name");
-let user_points = sessionStorage.getItem("points");
+window.addEventListener("DOMContentLoaded", function () {
+	// This block of code retrieves the "name" and "points" values from sessionStorage
+	// that were stored earlier during the quiz.
+	let user_name = sessionStorage.getItem("name");
+	let user_points = sessionStorage.getItem("points");
 
-document.querySelector("span.name").innerHTML = user_name;
-document.querySelector("span.points").innerHTML = user_points + "/12";
-
+	// This code updates the "name" and "points" spans in the HTML with the retrieved values.
+	document.querySelector("span.name").innerHTML = user_name;
+	document.querySelector("span.points").innerHTML = user_points + "/10";
+});
